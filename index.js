97d0cf2e384c2b6f463212e0bc764be805d8971d
Rich Harris <richard.a.harris@gmail.com>
2021-02-16 18:07:55 -0500

    use namespace import so that code breaks when you run it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // @ts-check
 import * as fs from 'fs';
-import d3Dsv from 'd3-dsv';
+import * as d3Dsv from 'd3-dsv';
 
 const csv = fs.readFileSync('my-data.csv', 'utf-8');
 const data = d3Dsv.csvParse(csv);
